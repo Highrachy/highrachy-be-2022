fa19f4764b6c1380b862b252686dc5b8509c14ec
Haruna Popoola <haruna.popoola@yahoo.com>
2022-06-11 21:43:04 +0100

    feature: add applicants

--- src/api/job/content-types/job/lifecycles.js
+++ src/api/job/content-types/job/lifecycles.js
@@ -1,16 +1,12 @@
 const slugify = require("slugify");
 
-const generateSlug = ({ title }) => slugify(title, { lower: true });
+const generateSlug = ({ title }) =>
+  `${slugify(title, { lower: true })}-${Date.now()}`;
 
 module.exports = {
   beforeCreate(event) {
     const { data } = event.params;
 
-    data.slug = generateSlug(data);
-  },
-
-  beforeUpdate(event) {
-    const { data } = event.params;
     data.slug = generateSlug(data);
   },
 };
