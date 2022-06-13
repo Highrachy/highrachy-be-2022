e988e8e0d617f8fce80a0f366d15f66f2cded745
Haruna Popoola <haruna.popoola@yahoo.com>
2022-06-13 18:16:36 +0100

    feature: add previous applicant policy

---
+++ src/api/applicant/policies/is-previous-applicant.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ * `is-previous-applicant` policy.
+ *
+ * https://docs.strapi.io/developer-docs/latest/development/backend-customization/policies.html#plugin-policies
+ */
+
+module.exports = async (policyContext, config, { strapi }) => {
+  const {
+    data: { email, job },
+  } = policyContext.request.body;
+
+  const previousRegistration = await strapi.entityService.findMany(
+    "api::applicant.applicant",
+    { filters: { email, job } }
+  );
+
+  return !previousRegistration.length > 0;
+};
--- src/api/applicant/routes/applicant.js
+++ src/api/applicant/routes/applicant.js
@@ -1,9 +1,18 @@
-'use strict';
+"use strict";
 
 /**
  * applicant router.
+ * https://docs.strapi.io/developer-docs/latest/development/backend-customization/routes.html#configuring-core-routers
+ *
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::applicant.applicant');
+module.exports = createCoreRouter("api::applicant.applicant", {
+  config: {
+    create: {
+      policies: ["is-previous-applicant"],
+      middlewares: [],
+    },
+  },
+});
